113b1caa3a5763affb0b6e82625b01464f036d47
wonderer007 <alihaider907@gmail.com>
2025-02-12 18:06:09 +0500

    test 3 pull requests

--- create_pr.sh
+++ create_pr.sh
@@ -5,13 +5,11 @@ BRANCH_REGEX="^([a-zA-Z]*\/)?[a-zA-Z]{2,3}-[0-9]+"
 BRANCH_PREFIX=$(echo "$BRANCH_NAME" | grep -oE "$BRANCH_REGEX")
 JIRA_TICKET=$(grep -oE '(?i)[a-zA-Z]{2,3}-[0-9]+' <<< "$BRANCH_PREFIX")
 REPO_NAME=$(basename "$PWD")
-BASE_BRANCH="main"
+TARGET_BRANCHES=("main" "ci-devel-server" "ci-stage-server")
 
 if [ -n "${JIRA_TICKET}" ]; then
     TICKET_TITLE=$(echo "${BRANCH_NAME#"$BRANCH_PREFIX"}" | sed -e 's/[-_]/ /g' -e 's/\b\(.\)/\u\1/g') | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)}1'
-    PR_TITLE="[$BASE_BRANCH, $REPO_NAME] $JIRA_TICKET: $(echo "$BRANCH_NAME" |echo "${TICKET_TITLE}")"
     COMMIT_MESSAGE=$(git log -1 --pretty=%B)
-
     JIRA_LINK="https://owenscorning.atlassian.net/browse/${JIRA_TICKET}"
 
     PR_DESCRIPTION=$(cat <<EOF
@@ -22,9 +20,13 @@ ${COMMIT_MESSAGE}
 EOF
     )
 
-    gh pr create --title "$PR_TITLE" --body "$PR_DESCRIPTION" --head "$BRANCH_NAME" --base "$BASE_BRANCH"
+    for BASE_BRANCH in "${TARGET_BRANCHES[@]}"; do
+        PR_TITLE="[$BASE_BRANCH, $REPO_NAME] $JIRA_TICKET: $(echo "$BRANCH_NAME" |echo "${TICKET_TITLE}")"
+        gh pr create --title "$PR_TITLE" --body "$PR_DESCRIPTION" --head "$BRANCH_NAME" --base "$BASE_BRANCH"
+        echo "Pull request created for $BASE_BRANCH!"
+    done
 
-    echo "Pull request created successfully!"
+    echo "All pull requests created successfully!"
 else
-  echo "Can not find JIRA ticket number"
+    echo "Can not find JIRA ticket number"
 fi
